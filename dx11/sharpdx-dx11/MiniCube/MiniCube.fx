15e9c298024a4ac82e0bfe4af293e5be74f15077
panda <zhonghaoyu555@gmail.com>
2022-03-23 01:52:15 +0800

    minicube

---
+++ dx11/sharpdx-dx11/MiniCube/MiniCube.fx
@@ -0,0 +1,33 @@
+﻿struct VS_IN
+{
+	float4 pos : POSITION;
+	float4 col : COLOR;
+};
+
+struct PS_IN
+{
+	float4 pos : SV_POSITION;
+	float4 ndc_coords : TEXCOORD0;
+	float4 col : COLOR;
+};
+
+float4x4 worldViewProj;
+
+PS_IN VS( VS_IN input )
+{
+	PS_IN output = (PS_IN)0;
+	
+	output.pos = mul(input.pos, worldViewProj);
+	output.ndc_coords = output.pos;
+	output.col = input.col;
+	
+	return output;
+}
+
+float4 PS(PS_IN input) : SV_Target
+{
+	//return input.col;
+	float3 ndc = input.ndc_coords.xyz / input.ndc_coords.w;
+	return float4(ndc.x * 0.5 + 0.5,ndc.y * 0.5 + 0.5,ndc.z,1.0);
+//	return float4(1.0,1.0,0.0,1.0);
+}
---
+++ dx11/sharpdx-dx11/MiniCube/Program.cs
@@ -0,0 +1,283 @@
+﻿// Copyright (c) 2010-2013 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// -----------------------------------------------------------------------------
+// Original code from SlimDX project.
+// Greetings to SlimDX Group. Original code published with the following license:
+// -----------------------------------------------------------------------------
+/*
+* Copyright (c) 2007-2011 SlimDX Group
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+using System;
+using System.Diagnostics;
+using SharpDX;
+using SharpDX.D3DCompiler;
+using SharpDX.Direct3D;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using SharpDX.Windows;
+using Buffer = SharpDX.Direct3D11.Buffer;
+using Device = SharpDX.Direct3D11.Device;
+
+namespace MiniCube
+{
+    /// <summary>
+    ///   SharpDX port of SharpDX-MiniCube Direct3D 11 Sample
+    /// </summary>
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            #region Direct3D Init 
+            var form = new RenderForm("SharpDX - MiniCube Direct3D 11 Sample");
+
+            // SwapChain description
+            var desc = new SwapChainDescription
+            {
+                BufferCount = 1,
+                ModeDescription =
+                                   new ModeDescription(form.ClientSize.Width, form.ClientSize.Height,
+                                                       new Rational(60, 1), Format.R8G8B8A8_UNorm),
+                IsWindowed = true,
+                OutputHandle = form.Handle,
+                SampleDescription = new SampleDescription(1, 0),
+                SwapEffect = SwapEffect.Discard,
+                Usage = Usage.RenderTargetOutput | Usage.BackBuffer,
+                Flags = SwapChainFlags.None
+            };
+
+            // Create Device and SwapChain
+            Device device;
+            SwapChain swapChain;
+#if DEBUG
+            var flags = DeviceCreationFlags.None;
+            Configuration.ThrowOnShaderCompileError = true;
+            Configuration.EnableObjectTracking = true;
+#else
+            var flags = DeviceCreationFlags.DEBUG;
+#endif
+            Device.CreateWithSwapChain(DriverType.Hardware, flags, new[] { FeatureLevel.Level_11_1, FeatureLevel.Level_11_0 }, desc, out device, out swapChain);
+            var context = device.ImmediateContext;
+
+            // Ignore all windows events
+            var factory = swapChain.GetParent<Factory>();
+            factory.MakeWindowAssociation(form.Handle, WindowAssociationFlags.IgnoreAll);
+
+
+            // Compile Vertex and Pixel shaders
+#if DEBUG
+            var shaderflags = ShaderFlags.Debug;
+#else
+            var shaderflags = ShaderFlags.None;
+#endif
+            var vertexShaderByteCode = ShaderBytecode.CompileFromFile("MiniCube.fx", "VS", "vs_4_0",shaderflags, EffectFlags.None);
+            var vertexShader = new VertexShader(device, vertexShaderByteCode);
+
+            var pixelShaderByteCode = ShaderBytecode.CompileFromFile("MiniCube.fx", "PS", "ps_4_0", shaderflags, EffectFlags.None);
+            var pixelShader = new PixelShader(device, pixelShaderByteCode);
+            #endregion
+
+            // Layout from VertexShader input signature
+            var layout = new InputLayout(
+                device,
+                ShaderSignature.GetInputSignature(vertexShaderByteCode),
+                new[]
+                    {
+                        new InputElement("POSITION", 0, Format.R32G32B32A32_Float, 0, 0),
+                        new InputElement("COLOR", 0, Format.R32G32B32A32_Float, 16, 0)
+                    });
+
+            // Instantiate Vertex buiffer from vertex data
+            var vertices = Buffer.Create(device, BindFlags.VertexBuffer, new[]
+                                  {
+                                      new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f), // Front
+                                      new Vector4(-1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                                      new Vector4( 1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                                      new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                                      new Vector4( 1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                                      new Vector4( 1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+
+                                      new Vector4(-1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f), // BACK
+                                      new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4(-1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4(-1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4( 1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+
+                                      new Vector4(-1.0f, 1.0f, -1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f), // Top
+                                      new Vector4(-1.0f, 1.0f,  1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+                                      new Vector4( 1.0f, 1.0f,  1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+                                      new Vector4(-1.0f, 1.0f, -1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+                                      new Vector4( 1.0f, 1.0f,  1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+                                      new Vector4( 1.0f, 1.0f, -1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+
+                                      new Vector4(-1.0f,-1.0f, -1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f), // Bottom
+                                      new Vector4( 1.0f,-1.0f,  1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4(-1.0f,-1.0f,  1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4(-1.0f,-1.0f, -1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4( 1.0f,-1.0f, -1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4( 1.0f,-1.0f,  1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+
+                                      new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), // Left
+                                      new Vector4(-1.0f, -1.0f,  1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+                                      new Vector4(-1.0f,  1.0f,  1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+                                      new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+                                      new Vector4(-1.0f,  1.0f,  1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+                                      new Vector4(-1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+
+                                      new Vector4( 1.0f, -1.0f, -1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f), // Right
+                                      new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+                                      new Vector4( 1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+                                      new Vector4( 1.0f, -1.0f, -1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+                                      new Vector4( 1.0f,  1.0f, -1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+                                      new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+
+                                  });
+
+            var constBuffer = new Buffer(device, Utilities.SizeOf<Matrix>(), ResourceUsage.Default, BindFlags.ConstantBuffer, CpuAccessFlags.None, ResourceOptionFlags.None, 0);
+
+            // New RenderTargetView from the backbuffer
+            //var backBuffer = Texture2D.FromSwapChain<Texture2D>(swapChain, 0);
+            //var renderTargetView = new RenderTargetView(device, backBuffer);
+
+            // Prepare All the stages
+            context.InputAssembler.InputLayout = layout;
+            context.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
+            context.InputAssembler.SetVertexBuffers(0, new VertexBufferBinding(vertices, Utilities.SizeOf<Vector4>() * 2, 0));
+            context.VertexShader.SetConstantBuffer(0, constBuffer);
+            context.VertexShader.Set(vertexShader);
+            context.PixelShader.Set(pixelShader);
+
+
+            var view = Matrix.LookAtLH(new Vector3(0, 0, -5), new Vector3(0, 0, 0), Vector3.UnitY);
+            Matrix proj = Matrix.Identity;
+            bool userResized = true;
+            Texture2D backBuffer = null;
+            RenderTargetView renderTargetView = null;
+            Texture2D depthStencilBuffer = null;
+            DepthStencilView depthStencilView= null;
+
+
+            form.UserResized += (sender, args) => userResized = true;
+            form.KeyUp += (sender, args) =>
+            {
+                if (args.KeyCode == System.Windows.Forms.Keys.F5)
+                    swapChain.SetFullscreenState(true, null);
+                else if (args.KeyCode == System.Windows.Forms.Keys.F4)
+                    swapChain.SetFullscreenState(false, null);
+                else if (args.KeyCode == System.Windows.Forms.Keys.Escape)
+                    form.Close();
+            };
+            var clock = new Stopwatch();
+            clock.Start();
+
+
+            // Main loop
+            RenderLoop.Run(form, () =>
+                                      {
+
+                                          #region Init SizeDependent resource
+                                          if(userResized)
+                                          {
+                                              Utilities.Dispose(ref backBuffer);
+                                              Utilities.Dispose(ref renderTargetView);
+                                              Utilities.Dispose(ref depthStencilBuffer);
+                                              Utilities.Dispose(ref depthStencilView);
+
+                                              swapChain.ResizeBuffers(desc.BufferCount, form.ClientSize.Width, form.ClientSize.Height, Format.Unknown, SwapChainFlags.None);
+                                              backBuffer = Texture2D.FromSwapChain<Texture2D>(swapChain, 0);
+                                              renderTargetView = new RenderTargetView(device, backBuffer);
+
+                                              depthStencilBuffer = new Texture2D(device, new Texture2DDescription
+                                              {
+                                                  Format = Format.D24_UNorm_S8_UInt,
+                                                  ArraySize = 1,
+                                                  MipLevels = 1,
+                                                  Width = form.ClientSize.Width,
+                                                  Height = form.ClientSize.Height,
+                                                  SampleDescription = new SampleDescription(1, 0),
+                                                  Usage = ResourceUsage.Default,
+                                                  BindFlags = BindFlags.DepthStencil,
+                                                  CpuAccessFlags = CpuAccessFlags.None,
+                                                  OptionFlags = ResourceOptionFlags.None
+                                              });
+                                              depthStencilView = new DepthStencilView(device, depthStencilBuffer);
+                                              context.Rasterizer.SetViewport(new Viewport(0, 0, form.ClientSize.Width, form.ClientSize.Height, 0.0f, 1.0f));
+                                              context.OutputMerger.SetTargets(depthStencilView,renderTargetView);
+                                              proj = Matrix.PerspectiveFovLH((float)Math.PI / 4.0f, form.ClientSize.Width / (float)form.ClientSize.Height, 0.1f, 100.0f);
+                                              userResized = false;
+                                          }
+                                          #endregion
+
+                                          var time = clock.ElapsedMilliseconds / 1000.0f;
+                                          var viewProj = Matrix.Multiply(view, proj);
+                                          context.ClearRenderTargetView(renderTargetView, Color.Black);
+                                          context.ClearDepthStencilView(depthStencilView, DepthStencilClearFlags.Depth, 1.0f, 0);
+
+                                          var World = Matrix.RotationX(time) * Matrix.RotationY(2 * time);
+                                          var MVP = World * viewProj;
+                                          MVP.Transpose();
+                                          // 在OpenGL里，所有向量都是列向量，矩阵乘法为左乘以，标准的MVP变换为 Proj * View * Model * vec4
+                                          // 在Dx里，所有的向量都是行向量，其乘法为 vec4 * M * V * P
+                                          // https://blog.csdn.net/weiyuxinyuan/article/details/78295969
+                                          //https://www.cnblogs.com/X-Jun/p/9808727.html
+                                          // hlsl有个优化就是 V * M V的第一个元素只与M的第一列有关，因此M矩阵可以保存为列主元的矩阵，这样可以一次性取出矩阵M的第一列的四个元素
+                                          //默认为行主序，因此要转置一下M
+                                          context.UpdateSubresource(ref MVP, constBuffer);
+                                          context.Draw(36, 0);
+                                          swapChain.Present(0, PresentFlags.None);
+                                      });
+
+            // Release all resources
+            vertexShaderByteCode.Dispose();
+            vertexShader.Dispose();
+            pixelShaderByteCode.Dispose();
+            pixelShader.Dispose();
+            vertices.Dispose();
+            layout.Dispose();
+            renderTargetView.Dispose();
+            backBuffer.Dispose();
+            context.ClearState();
+            context.Flush();
+            device.Dispose();
+            context.Dispose();
+            swapChain.Dispose();
+            factory.Dispose();
+        }
+    }
+}
