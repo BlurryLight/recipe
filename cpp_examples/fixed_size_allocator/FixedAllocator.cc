6b7060460e96d614d3d1b986c6bd46a7b3c0b5fc
panda <zhonghaoyu555@gmail.com>
2023-06-25 23:48:03 +0800

    support msvc

--- cpp_examples/fixed_size_allocator/FixedAllocator.cc
+++ cpp_examples/fixed_size_allocator/FixedAllocator.cc
@@ -3,6 +3,7 @@
 #include <memory>   // std::allocator
 #include <vector>
 #include <memory_resource>
+#include <array>
 
 // extented version
 // inspired by https://stackoverflow.com/a/72771645
@@ -47,6 +48,7 @@
 
 // compact version
 // https://en.cppreference.com/w/cpp/named_req/Allocator
+// https://stackoverflow.com/questions/48061522/create-the-simplest-allocator-with-two-template-arguments
 // check allocator completeness here 
 template <class T, size_t N> struct limited_allocator : std::allocator<T> {
   using value_type = typename std::allocator<T>::value_type;
@@ -55,6 +57,10 @@ template <class T, size_t N> struct limited_allocator : std::allocator<T> {
   template <class Other> struct rebind {
     typedef limited_allocator<Other, N> other;
   };
+
+  // make msvc happy
+  template <class Other,size_t M> 
+  constexpr operator limited_allocator<Other, M>() const noexcept { return limited_allocator<Other, M>(); }
 };
 
 template <class T, size_t N> struct limited_pmr_allocator : std::pmr::polymorphic_allocator<T> {
@@ -64,34 +70,48 @@ template <class T, size_t N> struct limited_pmr_allocator : std::pmr::polymorphi
   template <class Other> struct rebind {
     typedef limited_pmr_allocator<Other, N> other;
   };
+
+  template <class Other,size_t M> 
+  constexpr operator limited_pmr_allocator<Other, M>() const noexcept { return limited_pmr_allocator<Other, M>{this->resource()}; }
 };
 
 int main() {
-    auto al = limited_allocator<int,10>();
-    std::vector<int, limited_allocator<int,10>> v{};
-
-    auto vec  =v;
-
+  auto CheckVec = [](auto &vec, auto &al) {
     try {
-        vec.reserve(4);
-        vec.push_back(1);   // one element
-        vec.push_back(1);   // one element
-        vec.push_back(1);   // one element
-        vec.push_back(1);   // one element
-        vec.push_back(1);   // one element
-        vec.push_back(1);   // one element
-        for(int i = 0; i < 50;i++)
-        {vec.push_back(i);}
-    }
-    catch(const std::length_error& ex) {
-        std::cout << "length_error: " << ex.what() << '\n';
-    }    
-    catch(const std::bad_array_new_length& ex) {
-        std::cout << "bad_array_new_length: " << ex.what() << '\n';
-    }
-    catch(const std::bad_alloc& ex) {
-        std::cout << "bad_alloc: " << ex.what() << '\n';
+      vec.reserve(4);
+      vec.push_back(1); // one element
+      vec.push_back(1); // one element
+      vec.push_back(1); // one element
+      vec.push_back(1); // one element
+      vec.push_back(1); // one element
+      vec.push_back(1); // one element
+      for (int i = 0; i < 50; i++) {
+        vec.push_back(i);
+      }
+    } catch (const std::length_error &ex) {
+      std::cout << "length_error: " << ex.what() << '\n';
+    } catch (const std::bad_array_new_length &ex) {
+      std::cout << "bad_array_new_length: " << ex.what() << '\n';
+    } catch (const std::bad_alloc &ex) {
+      std::cout << "bad_alloc: " << ex.what() << '\n';
     }
     std::cout << vec.size() << std::endl;
-    std::cout << std::allocator_traits<decltype(al)>::max_size(al) << " " << vec.max_size() << std::endl;
+    std::cout << std::allocator_traits<std::remove_reference_t<decltype(al)>>::max_size(al) << " "
+              << vec.max_size() << std::endl;
+  };
+
+  {
+    auto al = limited_allocator<int, 10>();
+    std::vector<int, limited_allocator<int, 10>> vec{};
+    CheckVec(vec, al);
+  }
+
+  {
+
+    std::array<char, 1024 * 100> buffer{};
+    std::pmr::monotonic_buffer_resource resource(buffer.data(), buffer.size());
+    auto al = limited_pmr_allocator<int, 10>{&resource};
+    std::vector<int, limited_pmr_allocator<int, 10>> vec{al};
+    CheckVec(vec, al);
+  }
 }
